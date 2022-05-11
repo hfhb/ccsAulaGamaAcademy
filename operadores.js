b829cb94e461ae4a656dce3d7b3e0f5ea02c232b
Henrique F H Buongermino <henriquecontagames@gmail.com>
2022-05-11 17:22:09 -0300

    tetse de nova versao

---
+++ operadores.js
@@ -0,0 +1,65 @@
+/* operador condicional IF ELSE */
+
+/* var nome ='fernando'; 
+
+if (nome === 'henrique'){
+    console.log('parabens seu nome é muito bonito!!!');    
+} else if (nome === 'buongermino'){
+    console.log('seu nome tambêm é bonito!!!');
+} else {
+    console.log('desculpa este nome não serve!!!');
+}
+ */
+
+/*  operador condicional SWITCH CASE */
+ 
+/* var nome = prompt ("digite um nome: ");
+
+switch (nome) {
+    case 'henrique':
+        console.log('seja muito bem vindo');
+        break;
+    case 'fernando':
+        console.log('seja muito bem vindo');
+        break;
+    case 'honorio':
+        console.log('seja muito bem vindo');
+        break;
+    case 'buongermino':
+        console.log('seja muito bem vindo');
+        break;
+    default:
+        console.log('Você não esta autorizado');
+        break;
+
+}  */
+
+/* estrutura de repetição FOR */
+
+/* var tabuada = 7;
+
+for(var i = 0; i <= 10; i++){
+    console.log("Valor de" + tabuada + "x" + i + "=" + tabuada * i);    
+}
+ */
+
+
+/* var numSorte = 10;
+
+for(var i = 0; i <= 100; i++){
+    console.log("o valor de i = " + i );  
+    if(numSorte === i){
+        console.log("seu numero foi sorteado!!!!");
+        break;
+    }    
+}
+ */
+
+/* soteia numeros aleatorios */
+
+/* function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(  getRandomInt(0, 3)  );  // retornará 0, 1, 2 e 3.
+console.log(  getRandomInt(5, 10)  ); // retornará 5, 6, 7, 8, 9 e 10 */
